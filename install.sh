0e09ca21e9a78f9b01e480802afb63f1b733cce9
Jean M. Lescure <jeanmlescure@gmail.com>
2021-02-17 11:30:58 -0600

    fix: install script wrong registry

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ npm --version || (echo "ERROR: NPM required for installation" && exit 1)
 tar --version || (echo "ERROR: Tar required for installation" && exit 1)
 
 cd /tmp \
-&& npm --registry=https://registry.npmjs.org pack @GreenCubeIO/crew-mates \
+&& npm --registry=https://npm.pkg.github.com pack @GreenCubeIO/crew-mates \
 && sudo tar -xzf crew-mates-*.tgz --directory "/"
 
 if [ "$?" != "0" ]; then
