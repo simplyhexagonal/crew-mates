d954c8f8345d398ab9efa6c055cfde0aae8362fd
Jean M. Lescure <jeanmlescure@gmail.com>
2021-02-18 22:54:48 -0600

    feat: add ability to check crew-mates version

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 npm --version || (echo "ERROR: NPM required for installation" && exit 1)
 tar --version || (echo "ERROR: Tar required for installation" && exit 1)
+jq --version || (echo "ERROR: Crew Mates require jq" && exit 1)
+yarn --version || (echo "ERROR: Crew Mates require yarn" && exit 1)
+curl --version || (echo "ERROR: Crew Mates require curl" && exit 1)
 
 cd /tmp \
 && npm --registry=https://npm.pkg.github.com pack @GreenCubeIO/crew-mates@latest \
-&& sudo tar --exclude=package/package.json --strip-components=1 -xzf GreenCubeIO-crew-mates-*.tgz --directory "/"
+&& sudo tar --exclude=package/package.json --strip-components=1 -xzf GreenCubeIO-crew-mates-*.tgz --directory "/" \
+&& mkdir -p ~/.config/crew-mates \
+&& tar -xzf GreenCubeIO-crew-mates-*.tgz package/package.json --directory ~/.config/crew-mates/
 
 if [ "$?" != "0" ]; then
   exit 1
